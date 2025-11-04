aad9841221e52b8b16093612f72c40cd1f5f3eb3
MedAzizBouzid <80463714+MedAzizBouzid@users.noreply.github.com>
2025-11-04 15:26:15 +0100

    Linkedin Job scraping working

--- jobs-scraper/scraper.py
+++ jobs-scraper/scraper.py
@@ -13,7 +13,7 @@ async def agent_scraper_linkedin(query, location, num_pages=1):
     print("🕵️ Agent_Scraper (mode STEALTH 🥷): Launching for LinkedIn...")
 
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)
+        browser = await p.chromium.launch(headless=True)
         context = await browser.new_context(
             viewport={'width': 1920, 'height': 1080},
             user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
